422319fcc03a14100508f79f9dd23fce48f58ed9
Emirhan Çalışkan <emrhncalskn@gmail.com>
2022-02-16 22:04:05 +0300

    1-10'a kadar sayı miktarı kadar sayı bastırma

---
+++ console örnek2/Program.cs
@@ -0,0 +1,10 @@
+﻿string cikti = "";
+for (int i = 1; i < 10; i++)
+    for (int x = 0; x < i; x++)
+        cikti = cikti + i;
+
+Console.WriteLine(cikti);
+Console.ReadKey();
+
+
+//1-10'a kadar sayı miktarı kadar sayı bastırma
